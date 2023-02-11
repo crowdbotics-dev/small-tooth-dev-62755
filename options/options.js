3dded068fad835cf30baae2d83c3ec2bed7c33fd
Crowdbotics <engineering@crowdbotics.com>
2023-02-11 00:15:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-tooth-dev-62755",
+  url: "https://small-tooth-dev-62755.botics.co",
+  api: "https://small-tooth-dev-62755.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
